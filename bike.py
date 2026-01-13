9662e3b68c21eaa157356e06d079bdfd253623df
M-Ahmad-F25 <su92-msdsw-f25-013@superior.edu.pk>
2026-01-13 11:55:18 +0500

    Commit 2: Import ML Utilities and DL frameworks

--- bike.py
+++ bike.py
@@ -2,3 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from tensorflow import keras
+from tensorflow.keras import layers
